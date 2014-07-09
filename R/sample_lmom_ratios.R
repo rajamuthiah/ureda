43062f5f504f8046dcfd00e05c559ac865f5acf9
kokrah <kwame.okrah@gmail.com>
2014-07-09 13:06:13 -0400

    modified shape function

--- R/sample_lmom_ratios.R
+++ R/sample_lmom_ratios.R
@@ -1,114 +1,96 @@
-#' Compute the coefficients of the Betas (Internal function).
+#' Weight Coefficient for Sample L-moment. (Internal)
 #' 
-#' @param r the number of coefficients to be calculated (a whole number).
-#' @return a vector of coefficients of length r.
-pstar <- function(r){
+#' @param r The r-th sample l-moment.
+#' @param j The j-th term.
+#' @param n The sample size.
+#' @return The j-th weight factor of the r-th sample l-moment.
+weight.factor <- function (r, j, n) {
   
-  k <- 0:r
+  upper.limit <- min(j - 1, r - 1)
+  i <- 0:upper.limit
   
-  (-1)^(r - k) * choose(r, k) * choose(r + k, k)
+  factor1 <- (-1)^(r - 1 - i)
+  factor2 <- choose(r - 1, i)
+  factor3 <- choose(r - 1 + i, i)
+  factor4 <- choose(j - 1, i)
+  factor5 <- choose(n - 1, i)
+  
+  sum(factor1 * factor2 * factor3 * factor4 / factor5)
   
 }
 
-#' Compute a single term of the Betas (Internal function).
+#' Weight Coefficients for Sample L-moment (Internal).
 #' 
-#' @param x sample (numeric vector).
-#' @param k the k-th Beta term.
-#' @return a numeric vector of length 1 (the k-th Beta term).
-bterm <- function(x, k){
+#' @param r The r-th sample l-moment.
+#' @param n The sample size.
+#' @return A vector of weight factors.
+weightFactors <- function (r, n) {
   
-  n <- length(x) 
-  
-  orderd.x <- sort(x, decreasing = FALSE)
-  
-  j <- (k + 1):n
-  
-  (1 / n) * (1 / choose(n - 1, k)) * sum(choose(j - 1, k) * orderd.x[j])
+  sapply(1:n, weight.factor, r = r, n = n)
   
 }
 
-#' Compute all the Beta (Internal function).
+#' Weight Coefficients for Sample L-moments.
 #' 
-#' Vectorize internal.b
-#' @param x numeric vector (ie. sample data).
-#' @param r the of Betas terms.
-#' @return a numeric vector of length r (Betas).
-betas <- function(x, r){
+#' @param nlmom The number of sample l-moments.
+#' @param n The sample size.
+#' @return A matrix of weight factors.
+#' @export
+weightMatrix <- function (nlmom, n) {
   
-  sapply(0:r, bterm, x = x)
+  if (n < 1) {
+    stop("n must be greater than or equal to 1.")
+  }
   
-}  
-
-#' Compute the r-th l-statistic from a given sample (Internal function).
-#' 
-#' @param smpl sample (numeric vector).
-#' @param r the r-th sample l-statistic (a single whole number).
-#' @return a numeric vector of length 1 (the r-th l-statistic).
-singlelmom <- function(smpl, r){
+  if (nlmom < 1) {
+    stop("nlmom must be greater than or equal to 1.")
+  }
   
-  sum(betas(smpl, r - 1) * pstar(r - 1))
+  if (nlmom > n) {
+    stop("nlmom must be less than or equal to n.")
+  }
+  
+  res <- sapply(1:nlmom, weightFactors, n = n)
+  colnames(res) <- paste0("W", 1:nlmom)
+  
+  res
   
 }
 
 #' Sample L-moments ratios.
 #' 
-#' This function computes the l-moments ratios for a given
+#' This function computes the sample l-moments ratios for a given
 #' dataset (vector / matrix / data.frame).
-#' @param x sample (vector / matrix / data.frame).
-#' @param ratios Compute l-moments ratios instead of the raw l-moments? (logical, default = TRUE).
-#' @param n the number of l-moments (ratios) to compute. (a whole number, default = 4).
-#' @return a numeric vector of length n / matrix with 4 columns containing 
-#' sample l-moments / l-moments ratios.
+#' @param x Data (vector / matrix / data.frame).
+#' @param nlmom The number of l-moments (ratios) to compute (Default = 4).
+#' @param ratios Compute sample l-moments ratios ? (logical, Default = TRUE).
+#' @return A list holding sample l-ratios, sample l-moments, and weights.
 #' @export
-shape <- function(x, ratios = TRUE, n = 4){
+shape <- function(x,  nlmom = 4, ratios = TRUE){
   
-  tol <- .Machine$double.eps^0.5
-  is.natural <- n > tol & abs(n - round(n)) < tol
-  
-  if (!is.natural) {
-    
-    stop("n must be a natural number (ie. n = 1, 2, 3, ....).")
-    
+  if (!is.matrix(x)) {
+    x <- t(x) 
   }
   
-  if (is.vector(x)) {
+  n <- ncol(x)
+  
+  weight.mat <- weightMatrix(nlmom, n)
+  x <- t(apply(x, 1, sort))
+  lmoms <- (x %*% weight.mat) / n
+  colnames(lmoms) <- paste0("l", 1:nlmom)
+  
+  if (ratios & nlmom > 2) {
     
-    if (n > length(x)) {
-      
-      stop("n must be less than or equal to sample size (length of x).")
-      
-    }
-    
-    res <- sapply(1:n, singlelmom, smpl = x)
-    names(res) <- paste0("l", 1:n)
-    
-    if (ratios) {
-      
-      res[3:n] <- res[3:n] / res[2]  
-      names(res) <- c(paste0("l", 1:2), paste0("t", 3:n))
-    }
+    lrats <- lmoms
+    colnames(lrats) <- c(paste0("l", 1:2), paste0("t", 3:nlmom))
+    lrats[, 3:nlmom] <- lrats[, 3:nlmom] / lrats[, 2]
     
   }else{
     
-    if (n > ncol(x)) {
-      
-      stop("n must be less than or equal to sample size (ncol of x).")
-      
-    }
-    
-        
-    res <- t(apply(x, 1, function(y) sapply(1:n, singlelmom, smpl = y)))
-    colnames(res) <- paste0("l", 1:n)
-
-    if (ratios) {
-      
-      res[, 3:n] <-  res[, 3:n] / res[, 2]
-      colnames(res) <- c(paste0("l", 1:2), paste0("t", 3:n))
-      
-    }
+    lrats = NULL
     
   }
   
-  res
+  list(lrats = lrats, lmoms = lmoms, W = weight.mat)
   
 }
